150ff767cc7f06f6a8982b9ff15488ac6be0b17f
chenhefan <1193417657@qq.com>
2020-06-22 10:20:48 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    主页
+  </div>
+</template>
+
+<script>
+// import { login } from "../api/http";
+// export default {
+// async created() {
+//    let res =await login()
+//  },
+// }
+</script>
+
+<style>
+
+</style>
